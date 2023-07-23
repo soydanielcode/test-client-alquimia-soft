9ad8d407f0f0416b94db39fe838b008a16e438ff
soydanieldev <daniel-ggo@hotmail.com>
2023-07-22 19:22:43 -0500

    Init proyect

---
+++ src/main/java/com/alquimiasoft/com/TestClientAlquimiasoftApplication.java
@@ -0,0 +1,13 @@
+package com.alquimiasoft.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClientAlquimiasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestClientAlquimiasoftApplication.class, args);
+	}
+
+}

d656f32a6599277a9249a599ece18b99a89a0297
soydanielcode <daniel-ggo@hotmail.com>
2023-07-27 16:33:09 -0500

    Change database tables Client and BranchAddress

--- src/main/java/alquimiasoft/minegocio/testclient/database/init_databse.sql
+++ src/main/java/alquimiasoft/minegocio/testclient/database/init_databse.sql
@@ -1,17 +1,15 @@
-DROP TABLE IF EXISTS client;
-DROP TABLE IF EXISTS branch_address;
-
 CREATE TABLE client
 (
     id                    serial PRIMARY KEY,
+    status_type           VARCHAR(50),
     identification_type   VARCHAR(50),
     identification_number VARCHAR(13),
     names                 VARCHAR(100),
     email                 VARCHAR(50),
     cellphone             VARCHAR(20),
-    province              VARCHAR(50),
-    city                  VARCHAR(100),
-    address               VARCHAR(100)
+    province              varchar(255),
+    city                  varchar(255),
+    address               varchar(255)
 );
 
 CREATE TABLE branch_address
